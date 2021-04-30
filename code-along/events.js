59efcbb9a19f68d5a7e088245d5c6f848657637e
Lauren Hauge <lauren.hauge@kellogg.northwestern.edu>
2021-04-29 20:49:40 -0500

    v1

--- code-along/events.js
+++ code-along/events.js
@@ -1,19 +1,99 @@
-// Get a reference to the page title element from the DOM
+// // Get a reference to the page title element from the DOM
+// let pageTitleElement = document.querySelector(`.page-title`)
+
+// // Modify the page title element's contents
+// pageTitleElement.innerHTML = `Movies to watch!`
+
+// // Get a reference to the bulleted list of movies
+// let movieList = document.querySelector(`.movies-to-watch`)
+
+// // Add an new list item to the bulleted list of movies
+// movieList.insertAdjacentHTML(`beforeend`, `
+//   <li>Spaceballs</li>
+// `)
+
+// // Get a reference to the image
+// let image = document.querySelector(`img`)
+
+// // Add the `border-pink-500` class to the image element
+// image.classList.add(`border-pink-500`)
+
+// get a reference to the click me button 
+let clickMeButton = document.querySelector(`.click-me`)
+
+// event listener for click me button 
+clickMeButton.addEventListener(`click`, async function(event){
 let pageTitleElement = document.querySelector(`.page-title`)
 
 // Modify the page title element's contents
 pageTitleElement.innerHTML = `Movies to watch!`
+})
 
-// Get a reference to the bulleted list of movies
+// get ref to lik to add a movie
+let addMovieLink = document.querySelector(`.add-movie`)
+addMovieLink.addEventListener(`click`, async function(event){
+  // prevent link's default behaviors
+  event.preventDefault()
+
+  // Get a reference to the bulleted list of movies
 let movieList = document.querySelector(`.movies-to-watch`)
 
 // Add an new list item to the bulleted list of movies
 movieList.insertAdjacentHTML(`beforeend`, `
   <li>Spaceballs</li>
 `)
+})
 
+//get a reference to "zoom image button"
+let zoomImageButton = document.querySelector(`.zoom-image`)
+
+//event listener 
+zoomImageButton.addEventListener(`click`, async function(event){
+  
 // Get a reference to the image
 let image = document.querySelector(`img`)
 
 // Add the `border-pink-500` class to the image element
 image.classList.add(`border-pink-500`)
+
+//make image larger
+image.classList.toggle(`w-96`)
+image.classList.toggle(`w-full`)
+})
+
+//reference to  "change image"
+let changeImageButton = document.querySelector(`.change-image`)
+
+//event listener
+changeImageButton.addEventListener(`click`, async function(event){
+  //get reference 
+  let image = document.querySelector(`img`)
+
+  image.setAttribute(`src`, `../images/grogu2.jpg`)
+})
+
+//reference 
+let sayHiButton = document.querySelector(`.say-hi`)
+
+//event listener 
+sayHiButton.addEventListener(`click`, async function(event){
+  // stop default behavior 
+  event.preventDefault()
+
+  //reference 
+let firstNameInput = document.querySelector(`#first-name`)
+
+//get value in first name input 
+let firstName = firstNameInput.value 
+
+let greetElement = document.querySelector(`.greet`)
+
+if (firstName.length > 0){
+//create a sentence to put in greet element 
+let sentence = `Hi, ${firstName}`
+
+greetElement.innerHTML = sentence
+} else {
+  greetElement.innerHTML = ``
+}
+})
--- lab/week5.js
+++ lab/week5.js
@@ -1,7 +1,7 @@
 // Goal: Implement a weather application using data from an external API
 // - Signup for an api key @ https://weatherapi.com
 // - The API takes three inputs (querystring parameters)
-//   - key = your API key
+//   - key = 79bee9cec94646bea2111705213004
 //   - q = a location query (e.g. Chicago)
 //   - days = number of days of forecast data to return, between 1-10
 // - Example: https://api.weatherapi.com/v1/forecast.json?key=YOUR-API-KEY&q=Chicago&days=3
@@ -15,26 +15,58 @@
 
 window.addEventListener('DOMContentLoaded', async function() {
   // Get a reference to the "get weather" button
+  let getWeatherButton = document.querySelector(`.get-weather`)
 
   // When the "get weather" button is clicked:
+  getWeatherButton.addEventListener(`click`, async function(event){
 
     // - Ignore the default behavior of the button
+    event.preventDefault()
 
     // - Get a reference to the element containing the user-entered location
+    let locationInput = document.querySelector(`#location`)
 
     // - Get the user-entered location from the element's value
+    let location = locationInput.value
 
     // - Check to see if the user entered anything; if so:
+    if (location.length > 0) { 
 
       // - Construct a URL to call the WeatherAPI.com API
+      let url = `https://api.weatherapi.com/v1/forecast.json?key=79bee9cec94646bea2111705213004&q=${location}&days=3`
 
       // - Fetch the url, wait for a response, store the response in memory
+      let response = await fetch(url)
 
       // - Ask for the json-formatted data from the response, wait for the data, store it in memory
+      let json = await response.json()
 
       // - Write the json-formatted data to the JavaScript console
+      console.log(json)
 
       // - Store the interpreted location, current weather conditions, the forecast as three separate variables
+      let locationResult = json.location
+      let currentResult = json.current
+      let forecastResult = json.forecast 
 
       // - Continue the recipe yourself!
+
+      // reference current element
+      let currentElement = document.querySelector(`.current`)
+
+      //replace current element's contents
+      currentElement.innerHTML = `
+        <div class="text-center space-y-2">
+          <div class="font-bold text-3xl">Current Weather for ${locationResult.name}, ${locationResult.region}</div>
+          <div class="font-bold">
+            <img src="https://cdn.weatherapi.com/weather/64x64/day/116.png" class="inline-block">
+            <span class="temperature">60</span>° 
+            and
+            <span class="conditions">Partly Cloudy</span>
+          </div>
+        </div>
+      </div>
+      `
+    }
+    })
 })
